f6f0c0e513f3b4a40cfd6de030d10cbdb4602edf
suriya-wsl <suriyaprakhash@gmail.com>
2024-04-14 15:54:05 -0400

    added github actions, deploy made as client & updated logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+        // dev: false,
+    // distDir: 'dist',
+    output: 'export',
+    images: {unoptimized: true},
+    // poweredByHeader: false,
+      // Use the CDN in production and localhost for development - https://www.jsdelivr.com/github
+    assetPrefix: isProd ? 'https://cdn.jsdelivr.net/gh/suriyaprakhash/mockmanga@gh-pages' : undefined,
 }
 
 export default nextConfig;
